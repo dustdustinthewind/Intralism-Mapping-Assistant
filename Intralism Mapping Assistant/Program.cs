aec4c9601d1372298b22f4c449f68161bc998a7a
FRUP <flyingrabidunicornpig@gmail.com>
2020-02-22 13:36:02 -0800

    Fix culture info of program

--- Intralism Mapping Assistant/Program.cs
+++ Intralism Mapping Assistant/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +15,9 @@ namespace Intralism_Mapping_Assistant
         [STAThread]
         static void Main()
         {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+            CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
